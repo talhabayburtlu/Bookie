8e8588965670867cdad234f56f88b0e49dd72347
Talha <talha_bayburtlu2@hotmail.com>
2021-03-25 13:20:03 +0300

    Init commit.

---
+++ Bookie Backend/src/test/java/com/swe/bookie/BookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe.bookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
