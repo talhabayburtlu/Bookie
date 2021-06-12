ef6e7bf7aefaa100fd133e8d00b8d419a339b453
Ahmet Akıl <ahmet.deathoftime@gmail.com>
2021-06-12 17:43:09 +0300

    Improved UI

--- bookie-app/lib/views/book_details_view.dart
+++ bookie-app/lib/views/book_details_view.dart
@@ -1,4 +1,3 @@
-import 'package:bubble/bubble.dart';
 import 'package:bookie/models/comment.dart';
 import 'package:bookie/utils/ui_helpers.dart';
 import 'package:bookie/viewmodels/book_details_viewmodel.dart';
@@ -7,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 class BookDetailsView extends StatelessWidget {
+  final rowAlignment = MainAxisAlignment.start;
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<BookDetailsViewModel>.reactive(
@@ -30,79 +30,105 @@ class BookDetailsView extends StatelessWidget {
             width: double.infinity,
             child: SingleChildScrollView(
               physics: AlwaysScrollableScrollPhysics(),
-              child: Column(
-                children: [
-                  Image.network(
-                    model.selectedBook.imageThumbnailLink ?? BOOK_FALLBACK_URL,
-                    width: 160,
-                  ),
-                  verticalSpaceSmall,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Title: ",
-                        style: TextStyle(
-                            fontSize: FONT_SIZE_MEDIUM,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "${model.selectedBook.title}",
-                        style: TextStyle(
-                          fontSize: FONT_SIZE_MEDIUM,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Author: ",
-                          style: TextStyle(
-                              fontSize: FONT_SIZE_MEDIUM,
-                              fontWeight: FontWeight.bold)),
-                      Text("${model.selectedBook.author}",
-                          style: TextStyle(
-                            fontSize: FONT_SIZE_MEDIUM,
-                          ))
-                    ],
-                  ),
-                  if (model.selectedBook.subtitle != null)
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 4),
+                child: Column(
+                  children: [
+                    Image.network(
+                      model.selectedBook.imageThumbnailLink ??
+                          BOOK_FALLBACK_URL,
+                      width: 160,
+                    ),
+                    verticalSpaceSmall,
                     Row(
+                      mainAxisAlignment: rowAlignment,
                       children: [
                         Text(
-                          "Subtitle: ",
-                          style: TextStyle(fontSize: FONT_SIZE_MEDIUM),
-                        ),
-                        Text(
-                          "${model.selectedBook.subtitle}",
-                          style: TextStyle(fontSize: FONT_SIZE_MEDIUM),
-                        )
-                      ],
-                    ),
-                  verticalSpaceMedium,
-                  if (model.comments.isNotEmpty)
-                    Container(
-                      padding: EdgeInsets.only(left: 4, bottom: 2),
-                      child: Align(
-                        child: Text(
-                          "Comments:",
+                          "Title: ",
                           style: TextStyle(
                               fontSize: FONT_SIZE_MEDIUM,
-                              fontWeight: FontWeight.w500),
+                              fontWeight: FontWeight.bold),
                         ),
-                        alignment: Alignment.centerLeft,
-                      ),
-                    ),
-                  ListView.separated(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (ctx, i) => CommentWidget(
-                            comment: model.comments[i],
+                        Text(
+                          "${model.selectedBook.title}",
+                          style: TextStyle(
+                            fontSize: FONT_SIZE_MEDIUM,
                           ),
-                      separatorBuilder: (_, __) => Divider(),
-                      itemCount: model.comments.length)
-                ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: rowAlignment,
+                      children: [
+                        Text("Author: ",
+                            style: TextStyle(
+                                fontSize: FONT_SIZE_MEDIUM,
+                                fontWeight: FontWeight.bold)),
+                        Text("${model.selectedBook.author}",
+                            style: TextStyle(
+                              fontSize: FONT_SIZE_MEDIUM,
+                            ))
+                      ],
+                    ),
+                    if (model.selectedBook.subtitle != null)
+                      Row(
+                        mainAxisAlignment: rowAlignment,
+                        children: [
+                          Text("Subtitle: ",
+                              style: TextStyle(
+                                  fontSize: FONT_SIZE_MEDIUM,
+                                  fontWeight: FontWeight.bold)),
+                          Text(
+                            "${model.selectedBook.subtitle}",
+                            style: TextStyle(fontSize: FONT_SIZE_MEDIUM),
+                          )
+                        ],
+                      ),
+                    if (model.selectedBook.description != null)
+                      Row(
+                        mainAxisAlignment: rowAlignment,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Description: ",
+                            style: TextStyle(
+                                fontSize: FONT_SIZE_MEDIUM,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Flexible(
+                            child: Text(
+                              "${model.selectedBook.description}",
+                              style: TextStyle(fontSize: FONT_SIZE_MEDIUM),
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        ],
+                      ),
+                    verticalSpaceMedium,
+                    if (model.comments.isNotEmpty)
+                      Container(
+                        padding: EdgeInsets.only(left: 4, bottom: 2),
+                        child: Align(
+                          child: Text(
+                            "Comments:",
+                            style: TextStyle(
+                                fontSize: FONT_SIZE_MEDIUM,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          alignment: Alignment.centerLeft,
+                        ),
+                      ),
+                    ListView.separated(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (ctx, i) => CommentWidget(
+                              comment: model.comments[i],
+                            ),
+                        separatorBuilder: (_, __) => Divider(),
+                        itemCount: model.comments.length)
+                  ],
+                ),
               ),
             ),
           ),
